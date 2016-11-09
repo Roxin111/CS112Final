aff5d6bb48aa489959d3aa4040cdc387f7f38df9
Steven\Roxin <Huynst01@students.ipfw.edu>
2016-11-09 16:12:59 -0500

    Added constants and added a Random integer function

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Roxin on 10/31/2016.
+ */
+const Fortune1 = 0;
+const Fortune2 =1;
+const Fortune3 = 2;
+const Fortune4 = 3;
+const Fortune5 = 4;
+const Fortune6 = 5;
+const Fortune7 = 6;
+const Fortune8 = 7
+function RandomInt (low,high)
+{
+    return Math.floor(Math.random()*(high-low+1)) +low
+}
