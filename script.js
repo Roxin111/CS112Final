4c9956b6254f47f5e3d0fa3b8a2665ea4d33276f
Steven\Roxin <Huynst01@students.ipfw.edu>
2016-11-12 23:50:30 -0500

    added if and if else statements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * Created by Roxin on 10/31/2016.
  */
 const Fortune1 = 0;
-const Fortune2 =1;
+const Fortune2 = 1;
 const Fortune3 = 2;
 const Fortune4 = 3;
 const Fortune5 = 4;
@@ -15,6 +15,24 @@ function RandomInt (low,high)
 }
 function Displayresult ()
 {
-
-
+    Fortunenumber = RandomInt(0, 8);
+    if (Fortunenumber == Fortune1) {
+        summary = SESSION_PLAYER_WINS;
+        playerWins++;
+    } else if (Fortunenumber == PAPER && computerChoice == ROCK) {
+        summary = SESSION_PLAYER_WINS;
+        playerWins++;
+    } else if (Fortunenumber == SCISSORS && computerChoice == PAPER) {
+        summary = SESSION_PLAYER_WINS;
+        playerWins++;
+    } else if (Fortunenumber == ROCK && computerChoice == PAPER) {
+        summary = SESSION_COMPUTER_WINS;
+        computerWins++;
+    } else if (Fortunenumber == PAPER && computerChoice == SCISSORS) {
+        summary = SESSION_COMPUTER_WINS;
+        computerWins++;
+    } else if (Fortunenumber == SCISSORS && computerChoice == ROCK) {
+        summary = SESSION_COMPUTER_WINS;
+        computerWins++;
+    }
 }
