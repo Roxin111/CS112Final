ad933beb9e51f9215eb65f145718fe07d3a6d06b
Steven\Roxin <Huynst01@students.ipfw.edu>
2016-11-22 22:45:57 -0500

    Declared const

---
+++ script2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Roxin on 11/22/2016.
+ */
+const Fortune1 = 0;
+const Fortune2 = 1;
+const Fortune3 = 2;
+const Fortune4 = 3;
+const Fortune5 = 4;
+const Fortune6 = 5;
+const Fortune7 = 6;
+const Fortune8 = 7;
