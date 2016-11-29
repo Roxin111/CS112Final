377522c0d44903d7ec60282a61be4fb713e3b8a7
Steven\Roxin <Huynst01@students.ipfw.edu>
2016-11-29 12:06:38 -0500

    Added all constants for custom

--- script2.js
+++ script2.js
@@ -9,3 +9,38 @@ const Fortune5 = 4;
 const Fortune6 = 5;
 const Fortune7 = 6;
 const Fortune8 = 7;
+
+const displayFortune1 = "";
+const displayFortune2 = "";
+const displayFortune3 = "";
+const displayFortune4 = "";
+const displayFortune5 = "";
+const displayFortune6 = "";
+const displayFortune7 = "";
+const displayFortune8 = "";
+
+function $(value) {
+    return document.getElementById(value);
+}
+function DisplayResult()
+{
+    var Fortunenumber = RandomInt(0, 8);
+    if (Fortunenumber == Fortune1) {
+        $("Output").innerHTML=displayFortune1;
+    } else if (Fortunenumber == Fortune2) {
+        $("Output").innerHTML=displayFortune2;
+    } else if (Fortunenumber == Fortune3) {
+        $("Output").innerHTML=displayFortune3;
+    } else if (Fortunenumber == Fortune4) {
+        $("Output").innerHTML=displayFortune4;
+    } else if (Fortunenumber == Fortune5) {
+        $("Output").innerHTML=displayFortune5;
+    } else if (Fortunenumber == Fortune6) {
+        $("Output").innerHTML=displayFortune6;
+    }else if (Fortunenumber == Fortune7) {
+        $("Output").innerHTML=displayFortune7;
+    } else if (Fortunenumber == Fortune8) {
+        $("Output").innerHTML=displayFortune8;
+    }
+
+}
